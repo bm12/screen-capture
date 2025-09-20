6f19b547e2674a6bb8682b56ef7c7a3d020e1c77
Babaev Makhmoud <mahmud1997babaev@mail.ru>
2025-09-20 13:40:05 +0300

    Refactor server build to TypeScript

--- server/api/calls.test.ts
+++ server/api/calls.test.ts
@@ -1,9 +1,7 @@
 import request from 'supertest';
 import { describe, expect, it } from 'vitest';
 
-import serverModule from '../../server.js';
-
-const { app } = serverModule as { app: import('express').Express };
+import { app } from '#server';
 
 const uuidV4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
